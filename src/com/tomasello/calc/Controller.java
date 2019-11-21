67c548e83e1313ec3c158b677eed29af40babeef
ftomasello <32821413+franktomasello@users.noreply.github.com>
2019-11-20 16:29:28 -0800

    Initial commit

---
+++ src/com/tomasello/calc/Controller.java
@@ -0,0 +1,62 @@
+package com.tomasello.calc;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+	@FXML
+	private Text output;
+
+	private long number1 = 0;
+	private String operator = "";
+	private boolean start = true;
+
+	private Model model = new Model();
+
+	@FXML
+	private void processNumpad(ActionEvent event) {
+		if (start) {
+			output.setText("");
+			start = false;
+		}
+
+		String value = ((Button)event.getSource()).getText();
+		if(value.equals("CE"))
+		{
+			output.setText("");
+		}
+		else {
+			output.setText(output.getText() + value);
+		}
+	}
+
+	@FXML
+	private void processOperator(ActionEvent event) {
+		String value = ((Button)event.getSource()).getText();
+
+		if(value.equals("CE"))
+		{
+			output.setText("");
+		}
+
+		if (!"=".equals(value)) {
+			if (!operator.isEmpty())
+				return;
+
+			operator = value;
+			number1 = Long.parseLong(output.getText());
+			output.setText("");
+		}
+		else {
+			if (operator.isEmpty())
+				return;
+
+			output.setText(String.valueOf(model.calculate(number1, Long.parseLong(output.getText()), operator)));
+			operator = "";
+			start = true;
+		}
+	}
+}
